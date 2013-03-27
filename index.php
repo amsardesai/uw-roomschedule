bb68f77ab2f51f4860ffa613e91940fbc9a714cd
Ankit Sardesai <amsardesai@gmail.com>
2013-03-27 16:16:07 -0400

    Cleaning up a little

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php
 	header("Content-type: application/json;");
 
-	$ini = parse_ini_file("config.ini");
-
-	define("API_KEY", $ini["uwapikey"]);
-	$key = API_KEY;
-	$json = '';
-	if (($io=fsockopen('api.uwaterloo.ca', 80, $errno, $errstr, 5))) {
-		$sr  = "GET /public/v1/?key=$key&service=CourseFromRoom&q=EIT1015 HTTP/1.1\r\n";
-		$sr .= "Host: api.uwaterloo.ca\r\n";
-		$sr .= "Connection: close\r\n\r\n";
-		fputs($io, $sr);
-		$header = '';
-		do {
-			$header .= fgets($io, 128);
-		} while (strpos($header, "\r\n\r\n" ) === false);
-		while (!feof($io)) {
-			$json .= fgets($io, 128);
+	function getRoomData($building,$number) {
+		$ini = parse_ini_file("config.ini");
+		$uwkey = $ini["uwapikey"];
+		$building = strtoupper($building);
+		$body = '';
+		if (($io=fsockopen('api.uwaterloo.ca', 80, $errno, $errstr, 30))) {
+			$sr  = "GET /public/v1/?key=$uwkey&service=CourseFromRoom&q=$building$number HTTP/1.1\r\n";
+			$sr .= "Host: api.uwaterloo.ca\r\n";
+			$sr .= "Connection: close\r\n\r\n";
+			fputs($io, $sr);
+			$header = '';
+			do {
+				$header .= fgets($io, 128);
+			} while (strpos($header, "\r\n\r\n" ) === false);
+			while (!feof($io)) {
+				$body .= fgets($io, 128);
+			}
+			fclose($io);
+		} else {
+			die();
 		}
-		echo $json;
-		fclose($io);
+		return json_decode($body);
 	}
 
+	$json = getRoomData("EIT","1015");
+
+	var_dump($json);
 
 ?>
