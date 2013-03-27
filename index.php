f7f9fe3372683025ca1034aaa00d34b41032d1d9
Ankit Sardesai <amsardesai@gmail.com>
2013-03-27 19:00:24 -0400

    Added many error messages

--- index.php
+++ index.php
@@ -2,11 +2,30 @@
 	header("Content-type: application/json;");
 
 	function getRoomData($building,$number) {
+		if ($building=="" && $number=="") {
+			return array(
+				"error" => true,
+				"number" => 1,
+				"message" => "Nothing entered!",
+			);
+		} else if ($building=="" || strlen($building)>3) {
+			return array(
+				"error" => true,
+				"number" => 2,
+				"message" => "Invalid building abbreviation!",
+			);
+		} else if ($number=="" || $number==0 || strlen($building)>4) {
+			return array(
+				"error" => true,
+				"number" => 3,
+				"message" => "Invalid room number!",
+			);
+		}
 		$ini = parse_ini_file("config.ini");
 		$uwkey = $ini["uwapikey"];
 		$building = strtoupper($building);
 		$body = '';
-		if (($io=fsockopen('api.uwaterloo.ca', 80, $errno, $errstr, 30))) {
+		if (($io=@fsockopen('api.uwaterloo.ca', 80, $errno, $errstr, 30))) {
 			$sr  = "GET /public/v1/?key=$uwkey&service=CourseFromRoom&q=$building$number HTTP/1.1\r\n";
 			$sr .= "Host: api.uwaterloo.ca\r\n";
 			$sr .= "Connection: close\r\n\r\n";
@@ -20,68 +39,85 @@
 			}
 			fclose($io);
 		} else {
-			echo "$errno: $errstr";
-			die();
+			return array(
+				"error" => true,
+				"number" => 0,
+				"message" => "Networking Error $errno: $errstr",
+			);
 		}
 		return json_decode($body);
 	}
 
-	$json = getRoomData("EIT","1015");
 
-	echo "\n";
-	$errcode = $json->response->meta->Status;
-
-	if ($errcode=="200") { // OK
-		$classArray = $json->response->data->result;
-		$schedule = array();
-		foreach ($classArray as $obj) {
-			$days = $obj->Days;
-			$outDays = "";
-			for ($i=0;$i<strlen($days);$i++) {
-				if (substr($days,$i,2)=="Th") {
-					$outDays .= "4";
-					$i++;
-				} else if (substr($days,$i,1)=="M") { $outDays .= "1"; }
-				else if (substr($days,$i,1)=="T") { $outDays .= "2"; }
-				else if (substr($days,$i,1)=="W") { $outDays .= "3"; }
-				else if (substr($days,$i,1)=="F") { $outDays .= "5"; }
+	function processRoomData($json) {
+		if (is_array($json)) return $json;
+		$errcode = $json->response->meta->Status;
+		if ($errcode=="200") { // OK
+			$classArray = $json->response->data->result;
+			if (count($classArray)==0) {
+				return array(
+					"error" => true,
+					"number" => 4,
+					"message" => "No classes in this room!",
+				);
 			}
-			$instructor = $obj->Instructor;
-			if ($instructor!="") {
-				$a = explode(",",$instructor,2);
-				$instructor = "$a[1] $a[0]";
+			$schedule = array();
+			foreach ($classArray as $obj) {
+				$days = $obj->Days;
+				$outDays = "";
+				for ($i=0;$i<strlen($days);$i++) {
+					if (substr($days,$i,2)=="Th") {
+						$outDays .= "4";
+						$i++;
+					} else if (substr($days,$i,1)=="M") { $outDays .= "1"; }
+					else if (substr($days,$i,1)=="T") { $outDays .= "2"; }
+					else if (substr($days,$i,1)=="W") { $outDays .= "3"; }
+					else if (substr($days,$i,1)=="F") { $outDays .= "5"; }
+				}
+				$instructor = $obj->Instructor;
+				if ($instructor!="") {
+					$a = explode(",",$instructor,2);
+					$instructor = "$a[1] $a[0]";
+				}
+
+				$objSection = $obj->Section;
+				$type = $section = "";
+				if ($objSection!="") {
+					$a = explode(" ",$objSection,2);
+					$type = $a[0];
+					$section = $a[1];
+				}
+
+				$newSched = array(
+					"course" => "$obj->DeptAcronym $obj->Number",
+					"title" => $obj->Title,
+					"instructor" => $instructor,
+					"type" => $type,
+					"section" => $section,
+					"start" => str_replace(":","",$obj->EndTime),
+					"end" => str_replace(":","",$obj->StartTime),
+					"days" => $outDays,
+					"id" => $obj->ID,
+					"term" => $obj->Term,
+				);
+
+				$schedule[] = $newSched;
 			}
-
-			$objSection = $obj->Section;
-			$type = $section = "";
-			if ($objSection!="") {
-				$a = explode(" ",$objSection,2);
-				$type = $a[0];
-				$section = $a[1];
-			}
-
-
-			$newSched = array(
-				"course" => "$obj->DeptAcronym $obj->Number",
-				"title" => $obj->Title,
-				"instructor" => $instructor,
-				"type" => $type,
-				"section" => $section,
-				"start" => str_replace(":","",$obj->EndTime),
-				"end" => str_replace(":","",$obj->StartTime),
-				"days" => $outDays,
-				"id" => $obj->ID,
-				"term" => $obj->Term,
+			return $schedule;
+		} else {
+			return array(
+				"error" => true,
+				"number" => $errcode,
+				"message" => $json->response->meta->Message,
 			);
-
-			var_dump($obj);
-			echo "\n";
-
-			$schedule[] = $newSched;
 		}
-		var_dump($schedule);
-	} else {
-
 	}
+
+	$building = $_GET["b"];
+	$room = $_GET["r"];
+
+	$json = json_encode(processRoomData(getRoomData($building,$room)));
+	
+	echo $json;
 
 ?>
