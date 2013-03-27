a36e48694c0f55169d4a5571b2303602b964fa6b
Ankit Sardesai <amsardesai@gmail.com>
2013-03-27 19:26:34 -0400

    Set up HTML/jquery for front-end

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<script src="jquery.js" type="text/javascript"></script>
+	<script src="main.js" type="text/javascript"></script>
+</head>
+<body>
+
+<form>
+<input type="text" id="building" />
+<input type="text" id="room" />
+<input type="button" id="submit" value="Submit" />
+
+</form>
+
+
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,3 @@
+$("#submit").click(function() {
+	alert("hi");
+});
