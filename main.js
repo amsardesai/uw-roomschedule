d4e14563040962adef070392b2dfa95707868c14
Ankit Sardesai <amsardesai@gmail.com>
2013-03-29 00:56:46 -0400

    Basic functionality now works!

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var calStart = 3;
+var calStart = 8;
 var calEnd = 23;
+var calHourHeight = 50;
 var height = 0;
 
 function setUpCalendar() {
@@ -20,7 +21,10 @@ function setUpCalendar() {
 			.append($("<div>").addClass("week").attr("id","friday")));
 
 	for (var i=calStart-1;i<calEnd;i++) {
-		var sidetext = i+":00";
+		var sidetext;
+		if (i<12) sidetext = i+" am";
+		else if (i==12) sidetext = "noon";
+		else sidetext = (i-12)+" pm";
 		var sideobj = $("<div>").addClass("hour").html(sidetext);
 		if (i>calStart-1) $("#calendar #sidetime").append(sideobj);
 		$("#mainContent #calendar #weekcontainer .week").each(function(index) {
@@ -28,24 +32,25 @@ function setUpCalendar() {
 		});
 	}
 
-	height = $("#calendar .hour").outerHeight()
+	$("#calendar .hour").height(calHourHeight);
+	height = $("#calendar .hour").outerHeight();
 	var topheight = $("#calendar #toplabels").outerHeight();
 	$("#calendar").innerHeight(height*(calEnd-calStart+1)+topheight);
-
-	var hi;
-	addToCalendar(9,0,0,50,1,hi);
 
 }
 
 function processJSON(json) {
-	$("#output").html(" ");
+	//$("#output").html(" ");
 	if (json.error) {
 		alert("Error " + json.number + ": " + json.message);
 		return;
 	}
 
-	for (i=0;i<json.length;i++) {
-		newRow = $("<tr>")
+	// create calendar - move this code to processJSON later
+	setUpCalendar();
+
+	for (var i=0;i<json.length;i++) {
+/*		newRow = $("<tr>")
 			.append($("<td>").attr("name","course").html(json[i].course))
 			.append($("<td>").attr("name","title").html(json[i].title))
 			.append($("<td>").attr("name","type").html(json[i].type))
@@ -56,11 +61,32 @@ function processJSON(json) {
 			.append($("<td>").attr("name","days").html(json[i].days))
 			.append($("<td>").attr("name","term").html(json[i].term))
 			.append($("<td>").attr("name","id").html(json[i].id));
-		$("#output").append(newRow);
+		$("#output").append(newRow); */
+
+		var days = json[i].days;
+		for (var j=0;j<days.length;j++) {
+			var cur = parseInt(days.charAt(j));
+			addToCalendar(cur,json[i]);
+		}
+		
+
 	}
 }
 
-function addToCalendar(starthour, startminute, lengthhour, lengthminute, day, course) {
+function addToCalendar(day,course) {
+	var start = parseInt(course.start);
+	var end = parseInt(course.end);
+	var starthour = Math.floor(start/100);
+	var startminute = start % 100;
+	var endhour = Math.floor(end/100);
+	var endminute = end % 100;
+	var lengthhour = endhour - starthour;
+	var lengthminute = endminute - startminute;
+	if (lengthminute < 0) {
+		lengthhour--;
+		lengthminute += 60;
+	}
+
 	var entry = $("<div>").addClass("item");
 	var focus;
 	switch (day) {
@@ -77,10 +103,14 @@ function addToCalendar(starthour, startminute, lengthhour, lengthminute, day, co
 	}
 
 	var height = focus.outerHeight();
-	
 	focus.eq(starthour-calStart+1).html(entry);
+	var vOffset = height*(startminute/60);
+	var vHeight = height*(lengthhour+(lengthminute/60));
+	entry.css("top",vOffset);
+	entry.css("height",vHeight);
 
-
+	entry.append($("<span>").addClass("info").attr("id","coursename").html(course.course))
+		.append($("<span>").addClass("info").attr("id","coursetitle").html(course.title));
 
 
 
@@ -92,8 +122,7 @@ function addToCalendar(starthour, startminute, lengthhour, lengthminute, day, co
 
 $(document).ready(function() {
 
-	// create calendar - move this code to processJSON later
-	setUpCalendar();
+	
 
 	$("#submit").click(function(e) {
 		e.preventDefault();
